557641ac507cf6468a1224c71dae362e2c7a64c7
Ajay Kumar <ajay.ishan06@gmail.com>
2018-12-07 01:57:00 +0530

    Updated git ignore

---
+++ common-model/src/main/java/com/example/commonmodel/CommonModelApplication.java
@@ -0,0 +1,12 @@
+package com.example.commonmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonModelApplication.class, args);
+	}
+}
