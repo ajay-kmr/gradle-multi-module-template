83881a0b1a83ceee52ac6363c7d49ad62c192aba
Ajay Kumar <ajay.ishan06@gmail.com>
2019-01-03 02:30:08 +0530

    Code clean up

--- admin/src/test/java/com/example/admin/AdminApplicationTests.java
+++ admin/src/test/java/com/example/admin/AdminApplicationTests.java
@@ -1,16 +1,18 @@
 package com.example.admin;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AdminApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
